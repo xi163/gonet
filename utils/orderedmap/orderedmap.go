8284b6eb910318cbd9adbbf963e1fd48cf6ddacd
ro <luochengwu888@gmail.com>
2023-07-18 19:51:52 +0800

    commit

---
+++ utils/orderedmap/orderedmap.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"container/list"
+)
+
+// <summary>
+// Pair key-value
+// <summary>
+type Pair struct {
+	Key any
+	Val any
+}
+
+// <summary>
+// Orderedmap 排序map
+// <summary>
+type Orderedmap struct {
+	list *list.List
+}
+
+func NewOrderedmap() *Orderedmap {
+	return &Orderedmap{list: list.New()}
+}
+
+func (s *Orderedmap) Insert(key any, value any, compare func(a, b any) bool) {
+	pos := s.list.Front()
+	for ; pos != nil; pos = pos.Next() {
+		if !compare(key, pos.Value.(*Pair).Key) {
+			data := &Pair{Key: key, Val: value}
+			s.list.InsertBefore(data, pos)
+			break
+		}
+	}
+	if pos == nil {
+		data := &Pair{Key: key, Val: value}
+		s.list.PushBack(data)
+	}
+}
+
+func (s *Orderedmap) Top() (any, any) {
+	if elem := s.list.Front(); elem != nil {
+		data := elem.Value.(*Pair)
+		return data.Key, data.Val
+	}
+	return nil, nil
+}
+
+func (s *Orderedmap) Front() *list.Element {
+	return s.list.Front()
+}
+
+func (s *Orderedmap) Pop() {
+	if elem := s.list.Front(); elem != nil {
+		s.list.Remove(elem)
+	}
+}
+
+func (s *Orderedmap) Empty() bool {
+	return s.list.Len() == 0
+}
