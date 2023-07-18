76ac24d0a74ff737c80dff3f39d87332e70cd7e8
ro <luochengwu888@gmail.com>
2023-07-18 22:22:00 +0800

    commit

--- utils/Proto/proto.go
+++ utils/Proto/proto.go
@@ -1,17 +1,11 @@
 package Proto
 
-/*
-*
-struct/map/any编码成byte
-*/
-// func Encode(v any) ([]byte, error) {
-// 	return proto.Marshal(v)
-// }
+import "google.golang.org/protobuf/proto"
 
-/*
-*
-byte解码成struct/map/any
-*/
-// func Decode(a []byte, v any) error {
-// 	return proto.Unmarshal(a, v)
-// }
+func Encode(m proto.Message) ([]byte, error) {
+	return proto.Marshal(m)
+}
+
+func Decode(a []byte, m proto.Message) error {
+	return proto.Unmarshal(a, m)
+}
