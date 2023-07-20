c1775168467349b6a5906ff3afb91c8c24965b01
ro <luochengwu888@gmail.com>
2023-07-20 20:56:18 +0800

    commit

---
+++ utils/signal_handler/signal_test/signal_test.go
@@ -0,0 +1,23 @@
+package signal_test
+
+import (
+	"testing"
+
+	"github.com/cwloo/gonet/logs"
+	"github.com/cwloo/gonet/utils/signal_handler"
+)
+
+func TestMain(m *testing.M) {
+	m.Run()
+}
+
+func Test(t *testing.T) {
+	t.Run("signal_handler_test:", signal_handler_test)
+}
+
+func signal_handler_test(t *testing.T) {
+	signal_handler.RegisterStop(func() {
+		logs.Debugf("Stopping..")
+	})
+	signal_handler.Wait()
+}
