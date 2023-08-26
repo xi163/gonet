515c5d797d96aafb9829f7c13d8822d1d32291e0
ro <luochengwu888@gmail.com>
2023-08-26 14:33:29 +0800

    commit

--- core/base/run/catch.go
+++ core/base/run/catch.go
@@ -10,6 +10,6 @@ import (
 // 捕获panic内容并恢复程序运行，在panic之后触发，所以必须defer方式调用
 func Catch() {
 	if err := recover(); err != nil {
-		fmt.Fprint(os.Stderr, "panic: ", macro.SprintErrorf(6, "%v", err))
+		fmt.Fprint(os.Stderr, "run.panic: ", macro.SprintErrorf(6, "%v", err))
 	}
 }
--- logs/catch.go
+++ logs/catch.go
@@ -8,6 +8,6 @@ import (
 // 捕获panic内容并恢复程序运行，在panic之后触发，所以必须defer方式调用
 func Catch() {
 	if err := recover(); err != nil {
-		fmt.Fprint(os.Stderr, "panic: ", SprintErrorf(6, "%v", err))
+		fmt.Fprint(os.Stderr, "logs.panic: ", SprintErrorf(6, "%v", err))
 	}
 }
