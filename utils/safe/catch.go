10b45f334e55e6b75341271a4acb33db678a7e36
ro <luochengwu888@gmail.com>
2023-08-29 19:53:30 +0800

    commit

--- utils/safe/catch.go
+++ utils/safe/catch.go
@@ -8,6 +8,6 @@ import (
 // 捕获panic内容并恢复程序运行，在panic之后触发，所以必须defer方式调用
 func Catch() {
 	if err := recover(); err != nil {
-		logs.Errorf("safe.panic: ", macro.SprintErrorf(6, "%v", err))
+		logs.Errorf("safe.panic: %v", macro.SprintErrorf(6, "%v", err))
 	}
 }
